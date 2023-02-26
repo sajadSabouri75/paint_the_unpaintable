6de0ff649b9eee1d0f07b83f0e8b14603aa36c2a
sajad <sajad.sabouri.programming@gmail.com>
2023-02-26 16:59:15 +0330

    Canvas concept changed to handle only canvas related fields.

--- classes/canvas.py
+++ classes/canvas.py
@@ -1,20 +1,7 @@
+import numpy as np
 
 
 class Canvas:
     def __init__(self, **kwargs):
-        self._resolution = kwargs['resolution']
-        self._drops = []
-        self._simulation_time = kwargs['max_simulation_time']
-        self._fps = kwargs['fps']
-
-    # Add a new drop to the existing canvas
-    def add_new_drop(self, drop):
-        self._drops.append(drop)
-
-    # Simulate ripples due to water drops
-    def ripple(self):
-        pass
-
-    # Convert the simulated ripples to an array of raster images according to target fps
-    def convert_simulation_to_image(self):
-        pass
+        self._size = kwargs['size']
+        self._surface = np.zeros((self._size[0], self._size[1]))
